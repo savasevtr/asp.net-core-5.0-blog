1198802f6dbbb7b42c53d061b4fba986101dbd94
savasevtr <savas.ev@yahoo.com>
2021-05-22 19:08:09 +0300

    updated

---
+++ ProgrammersBlog.Data/Abstract/IArticleRepository.cs
@@ -0,0 +1,10 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Abstract;
+using System.Collections.Generic;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IArticleRepository : IEntityRepository<Article>
+    {
+    }
+}
