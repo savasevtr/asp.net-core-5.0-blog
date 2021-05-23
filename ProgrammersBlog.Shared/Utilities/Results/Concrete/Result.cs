a3b061cd842d12c16c315d92d79ef63a3012fe10
savasevtr <savas.ev@yahoo.com>
2021-05-23 22:47:13 +0300

    result

---
+++ ProgrammersBlog.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,31 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+
+namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
+{
+    public class Result : IResult
+    {
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+
+        public Result(ResultStatus resultStatus, string message, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+    }
+}
