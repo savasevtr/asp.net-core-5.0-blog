1022a3dd1339e8d3c1e8beb725e54be16702c38f
savasevtr <savas.ev@yahoo.com>
2021-05-23 11:41:37 +0300

    Unit Of Work Design Pattern

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveAsync();
+    }
+}
