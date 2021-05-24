ed9fb0f9d88e511756aab58c0a464102d29a9f25
savasevtr <savas.ev@yahoo.com>
2021-05-24 21:57:24 +0300

    CategoryManager added

---
+++ ProgrammersBlog.Entities/DTOs/CategoryAddDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammersBlog.Entities.DTOs
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+
+        [DisplayName("Açıklama")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Not")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir")]
+        public bool IsActive { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Entities/DTOs/CategoryUpdateDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammersBlog.Entities.DTOs
+{
+    public class CategoryUpdateDto
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+
+        [DisplayName("Açıklama")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Not")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden fazla olmamalıdır.")]
+        public string Note { get; set; }
+
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir")]
+        public bool IsActive { get; set; }
+
+        [DisplayName("Silindi Mi?")]
+        [Required(ErrorMessage = "{0} alanı boş geçilmemelidir.")]
+        public bool IsDeleted { get; set; }
+    }
+}
