2d4fc81e588090d354fb906bbd13f45232c4502b
savasevtr <savas.ev@yahoo.com>
2021-06-29 22:08:53 +0300

    updated

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProgrammersBlog.Entities.DTOs;
 using ProgrammersBlog.Mvc.Areas.Admin.Models;
@@ -10,6 +11,7 @@ using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin,Editor")]
     public class CategoryController : Controller
     {
         private readonly ICategoryService _categoryService;
@@ -22,23 +24,20 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
         public async Task<IActionResult> Index()
         {
             var result = await _categoryService.GetAllByNonDeleted();
-
             return View(result.Data);
-        }
 
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return PartialView("_CategoryAddPartial");
         }
-
         [HttpPost]
         public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
         {
             if (ModelState.IsValid)
             {
-                var result = await _categoryService.Add(categoryAddDto, "Savaş Ev");
-
+                var result = await _categoryService.Add(categoryAddDto, "Alper Tunga");
                 if (result.ResultStatus==ResultStatus.Success)
                 {
                     var categoryAddAjaxModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
@@ -46,42 +45,35 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
                         CategoryDto = result.Data,
                         CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
                     });
-
                     return Json(categoryAddAjaxModel);
                 }
             }
-            
             var categoryAddAjaxErrorModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
             {
                 CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
             });
-
             return Json(categoryAddAjaxErrorModel);
-        }
 
+        }
         [HttpGet]
         public async Task<IActionResult> Update(int categoryId)
         {
             var result = await _categoryService.GetCategoryUpdateDto(categoryId);
-
             if (result.ResultStatus==ResultStatus.Success)
             {
                 return PartialView("_CategoryUpdatePartial",result.Data);
             }
-
             else
             {
                 return NotFound();
             }
         }
-
         [HttpPost]
         public async Task<IActionResult> Update(CategoryUpdateDto categoryUpdateDto)
         {
             if (ModelState.IsValid)
             {
-                var result = await _categoryService.Update(categoryUpdateDto, "Savaş Ev");
-
+                var result = await _categoryService.Update(categoryUpdateDto, "Alper Tunga");
                 if (result.ResultStatus == ResultStatus.Success)
                 {
                     var categoryUpdateAjaxModel = JsonSerializer.Serialize(new CategoryUpdateAjaxViewModel
@@ -89,38 +81,32 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
                         CategoryDto = result.Data,
                         CategoryUpdatePartial = await this.RenderViewToStringAsync("_CategoryUpdatePartial", categoryUpdateDto)
                     });
-
                     return Json(categoryUpdateAjaxModel);
                 }
             }
-            
             var categoryUpdateAjaxErrorModel = JsonSerializer.Serialize(new CategoryUpdateAjaxViewModel
             {
                 CategoryUpdatePartial = await this.RenderViewToStringAsync("_CategoryUpdatePartial", categoryUpdateDto)
             });
-
             return Json(categoryUpdateAjaxErrorModel);
+
         }
 
         public async Task<JsonResult> GetAllCategories()
         {
             var result = await _categoryService.GetAllByNonDeleted();
-            
             var categories = JsonSerializer.Serialize(result.Data, new JsonSerializerOptions
             {
                 ReferenceHandler = ReferenceHandler.Preserve
             });
-
             return Json(categories);
         }
 
         [HttpPost]
         public async Task<JsonResult> Delete(int categoryId)
         {
-            var result = await _categoryService.Delete(categoryId, "Savaş Ev");
-            
+            var result = await _categoryService.Delete(categoryId, "Alper Tunga");
             var deletedCategory = JsonSerializer.Serialize(result.Data);
-
             return Json(deletedCategory);
         }
     }
--- ProgrammersBlog.Shared/Utilities/Extensions/DateTimeExtensions.cs
+++ ProgrammersBlog.Shared/Utilities/Extensions/DateTimeExtensions.cs
@@ -4,7 +4,7 @@ namespace ProgrammersBlog.Shared.Utilities.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static string FullDateAndTimeStringWithUnserscore(this DateTime dateTime)
+        public static string FullDateAndTimeStringWithUnderscore(this DateTime dateTime)
         {
             return $"{dateTime.Millisecond}_{dateTime.Second}_{dateTime.Minute}_{dateTime.Hour}_{dateTime.Day}_{dateTime.Month}_{dateTime.Year}";
         }
